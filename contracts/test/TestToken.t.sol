69d55d52270d7e0c8d2a00ca831dbd718821382a
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-02-18 12:52:47 +0100

    test

--- contracts/test/TestToken.t.sol
+++ contracts/test/TestToken.t.sol
@@ -11,12 +11,17 @@ contract TestTokenTest is Test {
     TestToken public token;
 
     function setUp() public {
-        token = new TestToken();
+        token = new TestToken(
+            "TestToken",
+            "TST",
+            1000000 ether,
+            address(this)
+        );
     }
 
     function test_transfer() public {
-        token.tranfer(0x57e17ec08f7b4a6eb8ef98b6c26ef90f5512fb67, 1 ether);
-        uint256 bal = token.balanceOf(0x57e17ec08f7b4a6eb8ef98b6c26ef90f5512fb67);
+        token.transfer(0x57E17Ec08f7b4a6eB8eF98b6C26Ef90f5512Fb67, 1 ether);
+        uint256 bal = token.balanceOf(0x57E17Ec08f7b4a6eB8eF98b6C26Ef90f5512Fb67);
         assertEq(bal, 1 ether);
     }
 }
